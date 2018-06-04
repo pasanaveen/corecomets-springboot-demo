7fde22f2515590f0f87b17d81fd515e96b3c4bb5
pasa naveen <pasa.naveen@gmail.com>
2018-06-03 21:35:08 -0400

    intial commit

---
+++ src/main/java/com/corecomets/springboot/corecometsdemo/CorecometsDemoApplication.java
@@ -0,0 +1,12 @@
+package com.corecomets.springboot.corecometsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorecometsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorecometsDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/corecomets/springboot/corecometsdemo/CorecometsDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.corecomets.springboot.corecometsdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CorecometsDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
